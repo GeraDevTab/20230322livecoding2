384dba5a9c62f28b4237051d6ee88b5bf809ed33
geradevtab <everythingissofast@gmail.com>
2023-03-22 17:47:57 -0600

    actividad de live coding 2 en pares

--- starter-files/script.js
+++ starter-files/script.js
@@ -11,3 +11,26 @@ const API_ENDPOINT = 'https://yesno.wtf/api';
  * 5. Optional: add loading/error states
  *
  */
+
+function ejecutarApi()
+{
+    let api = fetch('https://yesno.wtf/api').
+    then(respuesta => respuesta.json()).then((datos)=>mostrarinformacion(datos));
+
+
+}
+
+function mostrarinformacion(datos){
+    console.log(datos.answer);
+    let posicion = document.getElementById("revisar");
+    let h1 = document.createElement("h1");
+    h1.textContent =datos.answer;
+    posicion.append(h1);
+}
+
+//let posicion = document.getElementById("answer");
+
+let button = document.getElementById("button");
+button.addEventListener('click',ejecutarApi);
+
+ejecutarApi();
